5ccfa510440c8a407101a1a28a821401c5cc364b
Jelena Delic <jelenadelic10@gmail.com>
2023-03-10 14:56:02 +0100

    Izmene u konekciji zbog otklanjanja greske

--- db.php
+++ db.php
@@ -1,17 +1,16 @@
 <?php
 
     $host = "localhost";
-    $user = "root";
-    $pass = "";
+    $user = "jeldel";
+    $pass = "jeldel";
     $db = "prvi_domaci_iteh";
     
     $conn = new mysqli($host, $user, $pass, $db);
     
+
     if(!$conn)
     {
         die(mysqli_error($conn));  
     }
-
-// Koristimo negaciju da nam se ne bi na svakom prozoru gde smo includeovali db pojavljivalo da je uspesna konekcija
-
+    
 ?>
